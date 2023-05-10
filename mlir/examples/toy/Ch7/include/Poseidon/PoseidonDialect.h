96647bc4a053b06824ec6c246f7bbd1208920acd
pralaydas <daspralay5495@gmail.com>
2023-05-10 10:56:03 +0530

    added poseidon dialect and ops

---
+++ mlir/examples/toy/Ch7/include/Poseidon/PoseidonDialect.h
@@ -0,0 +1,16 @@
+//===- PoseidonDialect.h - Poseidon dialect -----------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef POSEIDON_POSEIDONDIALECT_H
+#define POSEIDON_POSEIDONDIALECT_H
+
+#include "mlir/IR/Dialect.h"
+
+#include "Poseidon/PoseidonOpsDialect.h.inc"
+
+#endif // POSEIDON_POSEIDONDIALECT_H
---
+++ mlir/examples/toy/Ch7/include/Poseidon/PoseidonOps.h
@@ -0,0 +1,21 @@
+//===- PoseidonOps.h - Poseidon dialect ops -----------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef POSEIDON_POSEIDONOPS_H
+#define POSEIDON_POSEIDONOPS_H
+
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/OpDefinition.h"
+#include "mlir/Interfaces/InferTypeOpInterface.h"
+#include "mlir/Interfaces/SideEffectInterfaces.h"
+
+#define GET_OP_CLASSES
+#include "Poseidon/PoseidonOps.h.inc"
+
+#endif // POSEIDON_POSEIDONOPS_H

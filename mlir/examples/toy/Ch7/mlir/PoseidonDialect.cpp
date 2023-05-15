04f0d560d8355c016f166300258f72f3a47345db
pralaydas <daspralay5495@gmail.com>
2023-05-15 16:28:55 +0530

    added conversion file: LowerToPoseidon v9

---
+++ mlir/examples/toy/Ch7/mlir/PoseidonDialect.cpp
@@ -0,0 +1,75 @@
+//===- PoseidonDialect.cpp - Poseidon Dialect Definition-------------------*- C++ -*-===//
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file defines poseidon dialect.
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/DialectImplementation.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/IR/Operation.h"
+#include "mlir/IR/OperationSupport.h"
+#include "llvm/ADT/TypeSwitch.h"
+#include "llvm/AsmParser/Parser.h"
+#include "llvm/IR/Attributes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Type.h"
+#include "llvm/Support/SourceMgr.h"
+#include "mlir/Transforms/InliningUtils.h"
+
+#include "Poseidon/PoseidonDialect.h"
+#include "Poseidon/PoseidonOps.h"
+
+using namespace mlir;
+using namespace poseidon;
+
+#include "Poseidon/PoseidonDialect.cpp.inc"
+
+//===----------------------------------------------------------------------===//
+// PoseidonDialect Interfaces
+//===----------------------------------------------------------------------===//
+
+namespace {
+struct PoseidonInlinerInterface : public DialectInlinerInterface {
+  using DialectInlinerInterface::DialectInlinerInterface;
+  bool isLegalToInline(Operation *call, Operation *callable,
+                       bool wouldBeCloned) const final {
+    return true;
+  }
+  bool isLegalToInline(Operation *, Region *, bool,
+                       IRMapping &) const final {
+    return true;
+  }
+  bool isLegalToInline(Region *dest, Region *src, bool wouldBeCloned,
+                       IRMapping &valueMapping) const final {
+    return true;
+  }
+};
+} // namespace
+
+//===----------------------------------------------------------------------===//
+// Poseidon dialect.
+//===----------------------------------------------------------------------===//
+
+void PoseidonDialect::initialize() {
+  addOperations<
+#define GET_OP_LIST
+#include "Poseidon/PoseidonOps.cpp.inc"
+      >();
+  addInterfaces<PoseidonInlinerInterface>();
+}
